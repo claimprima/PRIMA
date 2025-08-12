3c7acd2dd3fa8fce027fdfa96b763cfb41d20e8b
claimprima <evejzovic@gmail.com>
2025-08-12 11:18:48 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function claim() {
   setStatus("Sending claim transaction...");
 
   try {
-    const amount = Web3.utils.toWei("2000", "ether"); // 2000 PRIMA (18 decimals)
+    const amount = Web3.utils.toWei("50", "ether"); // 50 PRIMA (18 decimals)
 
     let gas;
     try {
